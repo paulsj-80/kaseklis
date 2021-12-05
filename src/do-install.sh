d4aa5cca08139bd6a6e8f8a852fa0d65ffd9a7ea
Pauls Jakovels <pauls.jakovels@gmail.com>
2021-12-05 10:50:01 +0200

    better error handling

--- src/do-install.sh
+++ src/do-install.sh
@@ -1,4 +1,3 @@
-set -e
-cp kaseklis /usr/bin
+cp kaseklis /usr/bin || exit 1
 kget=/usr/bin/kget
-(test ! -f $kget || sed -n '1{/^#kaseklis/p};q' $kget | grep -q . || (echo "Couldn't install shortcut, $kget exists" && false)) && (echo -e "#kaseklis\n/usr/bin/kaseklis get \$@" > $kget && chmod +x $kget)
+(test ! -f $kget || sed -n '1{/^#kaseklis/p};q' $kget | grep -q . || (echo "Couldn't install shortcut, $kget exists" && exit 2)) && (echo -e "#kaseklis\n/usr/bin/kaseklis get \$@" > $kget && chmod +x $kget) || exit 3
