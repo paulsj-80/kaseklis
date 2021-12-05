5f841f64125217c796785b36f0bfc8d8161e4f97
Pauls Jakovels <pauls.jakovels@gmail.com>
2021-12-05 10:56:07 +0200

    do-install.sh can be called without make

--- src/do-install.sh
+++ src/do-install.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 cp kaseklis /usr/bin || exit 1
 kget=/usr/bin/kget
 (test ! -f $kget || sed -n '1{/^#kaseklis/p};q' $kget | grep -q . || (echo "Couldn't install shortcut, $kget exists" && exit 2)) && (echo -e "#kaseklis\n/usr/bin/kaseklis get \$@" > $kget && chmod +x $kget) || exit 3
